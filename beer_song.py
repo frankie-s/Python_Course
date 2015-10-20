79abc567cdbe1edbb682a0aedae6d36574d3c9c9
frankie-s <frankie-s@users.noreply.github.com>
2015-10-20 08:38:54 -0400

    Create beer_song.py

---
+++ beer_song.py
@@ -0,0 +1,21 @@
+import sys
+from num2words import num2words
+
+def sing(beer_num):
+    while beer_num >= 2:
+        print(num2words(beer_num)+" bottles of beer on the wall!\n"+num2words(beer_num)+" bottles of beer!\nTake one down\nAnd pass it around\n"+num2words(beer_num)+" bottles of beer on the wall!\n")
+        beer_num = beer_num - 1
+    print("one bottle of beer on the wall!\none bottle of beer!\nTake one down\nAnd pass it around\nNo more bottles of beer on the wall!\n")
+
+
+def main(*argv):
+        arg = sys.argv[0:]
+        if len(arg) == 1:
+            beer_num = 99
+            sing(beer_num)
+        else:
+            beer_num = int(sys.argv[1])
+            sing(beer_num)
+
+
+main(sys.argv[0:])
