3336dd5d0b068f48892a2b63b7f90b6b387d0e65
frankie-s <frankie-s@users.noreply.github.com>
2015-10-20 09:19:08 -0400

    updated project
    Fixed incorrect logic in last line of verse. Added capitalization to the first word for each line.

--- beer_song.py
+++ beer_song.py
@@ -1,11 +1,12 @@
+__author__      = "Frankie S."
 import sys
 from num2words import num2words
 
 def sing(beer_num):
     while beer_num >= 2:
-        print(num2words(beer_num)+" bottles of beer on the wall!\n"+num2words(beer_num)+" bottles of beer!\nTake one down\nAnd pass it around\n"+num2words(beer_num)+" bottles of beer on the wall!\n")
+        print(num2words(beer_num).title()+" bottles of beer on the wall!\n"+num2words(beer_num).title()+" bottles of beer!\nTake one down\nAnd pass it around\n"+num2words(beer_num -1).title()+" bottles of beer on the wall!\n")
         beer_num = beer_num - 1
-    print("one bottle of beer on the wall!\none bottle of beer!\nTake one down\nAnd pass it around\nNo more bottles of beer on the wall!\n")
+    print("One bottle of beer on the wall!\nOne bottle of beer!\nTake one down\nAnd pass it around\nNo more bottles of beer on the wall!\n")
 
 
 def main(*argv):
