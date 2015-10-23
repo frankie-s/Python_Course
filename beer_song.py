ab88a9188a158eadad60da08d27657d9e836aa0d
frankie-s <frankie@localhost.localdomain>
2015-10-23 08:43:03 -0400

    fixed beer_song.py

--- beer_song.py
+++ beer_song.py
@@ -3,31 +3,36 @@ __author__ = "Frankie S."
 
 words1 = {1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine"}
 words2 = {0:"",1:"-One",2:"-Two",3:"-Three",4:"-Four",5:"-Five",6:"-Six",7:"-Seven",8:"-Eight",9:"-Nine"}
-words3 = {10:"Ten",11:"Eleven",12:"Twelve",13:"Thirthteen",14:"Fourteen",15:"Fifthteen",16:"Sixteen",17:"Seventeen",18:"Eightteen",19:"Ninteen"}
-words4 = {9:'Ninety',8:'Eighty',7:'Seventy',6:'Sixty',5:'Fifty',4:'Fourty',3:'Thirty',2:'Twenty'}
+words3 = {9:"Nine",10:"Ten",11:"Eleven",12:"Twelve",13:"Thirthteen",14:"Fourteen",15:"Fifthteen",16:"Sixteen",17:"Seventeen",18:"Eighteen",19:"Ninteen",20:"Twenty"}
+words4 = {9:'Ninety',8:'Eighty',7:'Seventy',6:'Sixty',5:'Fifty',4:'Fourty',3:'Thirty',2:'Twenty',1:"Ninteen"}
 
 def sing(beer_num):
-    while 20<= beer_num <=99:
+    while 21 <= beer_num <=99:
         tens = beer_num / 10
         ones = beer_num % 10
         answer_str = words4[tens] + words2[ones]
-        #next_answer_str = words4[tens] + words2[ones -1]
-        print(answer_str + " bottles of beer on the wall!\n" + answer_str + " bottles of beer!\nTake one down\nAnd pass it around\n" + answer_str + " bottles of beer on the wall!\n")
-        beer_num = beer_num - 1
+        next_beer = beer_num - 1
+        next_tens = next_beer / 10
+        next_ones = next_beer % 10
+        next_answer_str = words4[next_tens] + words2[next_ones]
+        print(answer_str + " bottles of beer on the wall!\n" + answer_str + " bottles of beer!\nTake one down\nAnd pass it around\n" + next_answer_str + " bottles of beer on the wall!\n")
+        beer_num = next_beer
 
-    while 10 <= beer_num <= 19:
-        tens = beer_num / 10
-        ones = beer_num % 10
-        answer_str = words4[tens] + words2[ones]
-        #next_answer_str = words4[tens] + words2[ones -1]
-        print(answer_str + " bottles of beer on the wall!\n" + answer_str + " bottles of beer!\nTake one down\nAnd pass it around\n" + answer_str + " bottles of beer on the wall!\n")
-        beer_num = beer_num - 1
+    while 10 <= beer_num <= 20:
+        answer_str = words3[beer_num]
+        next_beer = beer_num - 1
+        next_answer_str = words3[next_beer]
+        print(answer_str + " bottles of beer on the wall!\n" + answer_str + " bottles of beer!\nTake one down\nAnd pass it around\n" + next_answer_str + " bottles of beer on the wall!\n")
+        beer_num = next_beer
 
     while 2<= beer_num <= 10:
-        for x in word_dic1:
-            print(word_dic1.get(x) +" bottles of beer on the wall!\n" +word_dic1.get(x) + " bottles of beer!\nTake one down\nAnd pass it around\n" +word_dic1.get(x) +" bottles of beer on the wall!\n")
-        beer_num = beer_num - 1
-        print("One bottle of beer on the wall!\nOne bottle of beer!\nTake one down\nAnd pass it around\nNo more bottles of beer on the wall!\n")
+        answer_str = words1[beer_num]
+        next_beer = int(beer_num -1)
+        next_answer_str = words1[next_beer]
+        print(answer_str +" bottles of beer on the wall!\n" + answer_str + " bottles of beer!\nTake one down\nAnd pass it around\n" + next_answer_str +" bottles of beer on the wall!\n")
+        beer_num = next_beer
+
+    print("One bottle of beer on the wall!\nOne bottle of beer!\nTake one down\nAnd pass it around\nNo more bottles of beer on the wall!\n")
 
 
 def main(*argv):
